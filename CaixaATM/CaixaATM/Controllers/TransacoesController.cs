91ff3c4431d76c45d0f9c1774592291e49ff3cea
Diego13pa <diego.pa13@hotmail.com>
2016-02-05 02:58:12 -0200

    Adicao de telas e inclusao de operacao sem login na home

---
+++ CaixaATM/CaixaATM/Controllers/TransacoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaixaATM.Controllers
+{
+    public class TransacoesController : Controller
+    {
+        // GET: Transacoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Saldo()
+        {
+            return View();
+        }
+
+        public ActionResult ExibicaoSaldo()
+        {
+            return View();
+        }
+
+        public ActionResult Extrato()
+        {
+            return View();
+        }
+
+        public ActionResult ExtratoPeriodo()
+        {
+            return View();
+        }
+
+        public ActionResult Saque()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmacaoSaque()
+        {
+            return View();
+        }
+
+        public ActionResult Deposito()
+        {
+            return View();
+        }
+
+        public ActionResult Transferencia()
+        {
+            return View();
+        }
+
+        public ActionResult TransDetalhes()
+        {
+            return View();
+        }
+
+        public ActionResult TransVerifica()
+        {
+            return View();
+        }
+
+        public ActionResult TransConfirma()
+        {
+            return View();
+        }
+
+        public ActionResult Cheque()
+        {
+            return View();
+        }
+
+        public ActionResult Investimento()
+        {
+            return View();
+        }
+
+        public ActionResult Rendimento()
+        {
+            return View();
+        }
+
+        public ActionResult ConfInvestimento()
+        {
+            return View();
+        }
+
+        public ActionResult Pagamentos()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoCodigo()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoVlrVenc()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoVencido()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoVerifica()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoConfirma()
+        {
+            return View();
+        }
+
+        public ActionResult Autenticacao()
+        {
+            return View();
+        }
+    }
+}
