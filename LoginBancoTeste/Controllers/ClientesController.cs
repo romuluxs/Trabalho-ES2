01af9261ed528fb6308af37cc69b426513660978
Romulo <romuluxs@gmail.com>
2016-02-20 17:20:33 -0200

    Mudança no Layout de Admin
    Mudança no Layout no ambiente administrativo, mudança nos estilos dos
    botões e etc...

--- LoginBancoTeste/Controllers/ClientesController.cs
+++ LoginBancoTeste/Controllers/ClientesController.cs
@@ -88,7 +88,7 @@ namespace LoginBancoTeste.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nome")] Cliente cliente)
+        public ActionResult Edit(Cliente cliente)
         {
             if (ModelState.IsValid)
             {
